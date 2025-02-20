4aa3d805df76b90dcd9be20833c290f8cdc52269
JHGiardina <JHGiardina@gmail.com>
2025-02-19 23:50:31 -0600

    AI!
    Added:
    State machine framework
    Patrol state
    Basic enemy

---
+++ Assets/Scripts/Enemy/EnemyPath.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class EnemyPath : MonoBehaviour
+{
+    public List<Transform> waypoints = new List<Transform>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
